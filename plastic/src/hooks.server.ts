c3c7f735c5f041f5747c4ba8e6643809e4510e14
BusinessJoe <lukasboelling002@gmail.com>
2024-03-31 13:36:55 -0400

    Set instead of append

--- plastic/src/hooks.server.ts
+++ plastic/src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const response = await resolve(event);
-	response.headers.append('Cross-Origin-Opener-Policy', 'same-origin');
-	response.headers.append('Cross-Origin-Embedder-Policy', 'require-corp');
+	response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
+	response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
 	return response;
 };
